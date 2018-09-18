629dfff7228003089edef7eab3910a8832810a5c
khaled <elgendy1204@gmail.com>
2018-09-18 14:30:40 +0200

    Fix ball movement

--- src/ball.js
+++ src/ball.js
@@ -4,7 +4,10 @@ export default class Ball {
     constructor() {
         this.element = document.getElementById('ball');
         this.diameter = 30;
+        this.XDirection = 1;
+        this.YDirection = 1;
         this.setInitialPosition();
+        this.move();
     }
 
     setInitialPosition() {
@@ -29,5 +32,36 @@ export default class Ball {
 
     renderPosition() {
         this.element.style.transform = `translate(${this.left}px, ${this.top}px)`;
+    }
+
+    move() {
+        const maxLeft = gameWidth - (gameBorder * 2) - this.diameter;
+        const maxTop = gameHeight - (gameBorder * 2) - this.diameter;
+
+        let XIncrementValue = 15;
+        let YIncrementValue = 5;
+
+        const top = this.top + (YIncrementValue * this.YDirection);
+        const left = this.left + (XIncrementValue * this.XDirection);
+
+        if(top > maxTop) {
+            this.YDirection = -1;
+        } else if (top < 0) {
+            this.YDirection = 1;
+        }
+
+        if(left > maxLeft) {
+            this.XDirection = -1;
+        } else if (left < 0) {
+            this.XDirection = 1;
+        }
+
+        const newPosition = {
+            top: this.top + YIncrementValue * this.YDirection,
+            left: this.left + XIncrementValue * this.XDirection
+        };
+
+        this.setPosition(newPosition);
+        requestAnimationFrame(this.move.bind(this));
     }
 };
